858d8b751245f3b1a6d77214b6b642d72eaa9587
Heitor Novais <hnovais95@gmail.com>
2021-06-14 11:43:57 -0300

    feat(projeto CleanApp): implementar teste para garantir url correta no adaptador do Alamofire

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -6,9 +6,58 @@
 //
 
 import XCTest
+import Alamofire
+
+class AlamofireAdapter {
+    
+    private let session: Session
+    
+    init(session: Session = .default) {
+        self.session = session
+    }
+    
+    func post(to url: URL) {
+        session.request(url).resume()
+    }
+}
 
 class AlamofireAdapterTests: XCTestCase {
     
-    func test_()
+    func test_() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+}
 
+class UrlProtocolStub: URLProtocol {
+    
+    static var notify: ((URLRequest) -> Void)?
+    
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.notify = completion
+    }
+    
+    open class override func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    open class override func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    open override func startLoading() {
+        UrlProtocolStub.notify?(request)
+    }
+    
+    open override func stopLoading() {}
 }
