1f50b527607c75803b054184667129941136a418
Heitor Novais <hnovais95@gmail.com>
2021-06-14 14:07:35 -0300

    feat(projeto CleanApp): implementar teste para garantir que Alamofire pode ser chamado com body sem dados

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -17,25 +17,43 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+        var json: [String: Any]?
+        if let data = data {
+            json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+        }
         session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
 class AlamofireAdapterTests: XCTestCase {
     
-    func test_post_should_make_valid_request_with_valid_url_and_method() {
+    func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
         let sut = AlamofireAdapter(session: session)
-        sut.post(to: url, with: Data())
+        sut.post(to: url, with: makeValidData())
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observeRequest { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
-            XCTAssertEqual(makeValidData(), Data(reading: request.httpBodyStream ?? InputStream()))
+            XCTAssertEqual(makeValidData(), Data(reading: request.httpBodyStream!))
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_post_should_make_request_with_no_data() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url, with: nil)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
