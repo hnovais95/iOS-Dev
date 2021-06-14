a30cf0d282fa48886099ed38904b2bcb72ca3c78
Heitor Novais <hnovais95@gmail.com>
2021-06-14 11:01:31 -0300

    feat(projeto CleanApp): adicionar camada de infraestrutura

---
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Heitor Novais | Gerencianet on 14/06/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    
+    func test_()
+
+}
