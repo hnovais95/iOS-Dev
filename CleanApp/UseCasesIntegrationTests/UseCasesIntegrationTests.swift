6d0c577e1c938a7035146eb8d67a61c55b6ee1d1
Heitor Novais <hnovais95@gmail.com>
2021-06-15 11:11:20 -0300

    feat(projeto CleanApp): adicionar teste de integração com a API

---
+++ CleanApp/UseCasesIntegrationTests/UseCasesIntegrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  UseCasesIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Heitor Novais | Gerencianet on 15/06/21.
+//
+
+import XCTest
+import Data
+import Infra
+import Domain
+
+class UseCasesIntegrationTests: XCTestCase {
+
+    func test_add_acount() {
+        let alamofireAdapter = AlamofireAdapter()
+        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        let addAccountModel = AddAccountModel(name: "Gorbadock Oldbuck", email: "any_email@domain.com" , password: "secret", passwordConfirmation: "secret")
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            switch result {
+            case .success(let account):
+                XCTAssertNotNil(account.id)
+                XCTAssertEqual(account.name, addAccountModel.name)
+                XCTAssertEqual(account.email, addAccountModel.email)
+            case .failure:
+                XCTFail("Expect success got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5)
+    }
+}
