3416494c94eef1b76a385eb5b58e1a2c09e1aabc
Heitor Novais <hnovais95@gmail.com>
2021-06-15 15:13:34 -0300

    feat(projeto CleanApp): adicionar teste para garantir mensagem de erro se campo nome não for fornecido

--- CleanApp/PresentationTests/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/SignUpPresenterTests.swift
@@ -7,9 +7,67 @@
 
 import XCTest
 
+class SignUpPresenter {
+    
+    private let alertView: AlertView
+    
+    init(alertView: AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signUp(viewModel: SignUpViewModel) {
+        if (viewModel.name == nil) || viewModel.name!.isEmpty {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório."))
+        }
+    }
+}
+
+protocol AlertView {
+    
+    func showMessage(viewModel: AlertViewModel)
+}
+
+struct AlertViewModel: Equatable {
+    
+    var title: String
+    var message: String
+}
+
+struct SignUpViewModel {
+    
+    var name: String?
+    var email: String?
+    var password: String?
+    var passwordConfirmation: String?
+}
+
 class SignUpPresenterTests: XCTestCase {
 
-    func testExample() {
+    func test_signUp_should_show_error_message_if_not_privaded() {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let signUpViewModel = makeInvalidViewModel()
+        sut.signUp(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório."))
+    }
+}
+
+extension SignUpPresenterTests {
     
+    func makeValidViewModel() -> SignUpViewModel {
+        return SignUpViewModel(name: "Gorbadock Oldbuck", email: "any_email@domain.com" , password: "secret", passwordConfirmation: "secret")
+    }
+    
+    func makeInvalidViewModel() -> SignUpViewModel {
+        return SignUpViewModel(email: "any_email@domain.com" , password: "secret", passwordConfirmation: "secret")
+    }
+    
+    class AlertViewSpy: AlertView {
+        
+        var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel: AlertViewModel) {
+            self.viewModel = viewModel
+        }
     }
 }
