bbbed00dbd67243a7f6fc9c789724ab133a5b10b
Heitor Novais <hnovais95@gmail.com>
2021-06-16 13:23:22 -0300

    feat(projeto CleanApp): adicionar camada de UI

---
+++ CleanApp/UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Heitor Novais | Gerencianet on 16/06/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+    }
+}
