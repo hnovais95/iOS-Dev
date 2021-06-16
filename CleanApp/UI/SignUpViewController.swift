78cac0b526ba170f3b1701cf087f8f859b74f2ed
Heitor Novais <hnovais95@gmail.com>
2021-06-16 14:04:36 -0300

    feat(projeto CleanApp): adicionar teste para garantir que active indicator não estará visível quando tela iniciar

---
+++ CleanApp/UI/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Heitor Novais | Gerencianet on 16/06/21.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
