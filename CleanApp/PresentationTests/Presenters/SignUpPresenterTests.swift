c3d3d534fc1f036320b0c87400f01563932bf372
Heitor Novais <hnovais95@gmail.com>
2021-06-16 09:28:58 -0300

    refactor(projeto CleanApp): refatorar criação do SignUpViewModel

--- CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -13,47 +13,42 @@ class SignUpPresenterTests: XCTestCase {
     func test_signUp_should_show_error_message_if_name_not_privaded() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let signUpViewModel = makeViewModelWithoutName()
-        sut.signUp(viewModel: signUpViewModel)
+        sut.signUp(viewModel: makeSignUpViewModel(name: nil))
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Nome é obrigatório."))
     }
     
     func test_signUp_should_show_error_message_if_email_not_privaded() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let signUpViewModel = makeViewModelWithoutEmail()
-        sut.signUp(viewModel: signUpViewModel)
+        sut.signUp(viewModel: makeSignUpViewModel(email: nil))
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Email é obrigatório."))
     }
     
     func test_signUp_should_show_error_message_if_password_not_privaded() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let signUpViewModel = makeViewModelWithoutPassword()
-        sut.signUp(viewModel: signUpViewModel)
+        sut.signUp(viewModel: makeSignUpViewModel(password: nil))
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Senha é obrigatório."))
     }
     
     func test_signUp_should_show_error_message_if_password_confirmation_not_privaded() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let signUpViewModel = makeViewModelWithoutPasswordConfirmation()
-        sut.signUp(viewModel: signUpViewModel)
+        sut.signUp(viewModel: makeSignUpViewModel(passwordConfirmation: nil))
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "O campo Confirmar Senha é obrigatório."))
     }
     
     func test_signUp_should_show_error_message_if_password_confirmation_not_match() {
         let alertViewSpy = AlertViewSpy()
         let sut = makeSut(alertView: alertViewSpy)
-        let signUpViewModel = makeViewModelWithPasswordConfirmationNotMatch()
-        sut.signUp(viewModel: signUpViewModel)
+        sut.signUp(viewModel: makeSignUpViewModel(passwordConfirmation: "wrong_password"))
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Falha ao confirmar senha."))
     }
     
     func test_signUp_should_call_emailValidator_with_correct_email() {
         let emailValidatorSpy = EmailValidatorSpy()
         let sut = makeSut(emailValidator: emailValidatorSpy)
-        let signUpViewModel = makeValidViewModel()
+        let signUpViewModel = makeSignUpViewModel()
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(emailValidatorSpy.email, signUpViewModel.email)
     }
@@ -62,9 +57,8 @@ class SignUpPresenterTests: XCTestCase {
         let alertViewSpy = AlertViewSpy()
         let emailValidatorSpy = EmailValidatorSpy()
         let sut = makeSut(alertView: alertViewSpy, emailValidator: emailValidatorSpy)
-        let signUpViewModel = makeViewModelWithInvalidEmail()
-        emailValidatorSpy.isValid = false
-        sut.signUp(viewModel: signUpViewModel)
+        emailValidatorSpy.simulateInvalidEmail()
+        sut.signUp(viewModel: makeSignUpViewModel())
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Falha na validação", message: "Email inválido."))
     }
 }
@@ -76,42 +70,22 @@ extension SignUpPresenterTests {
         return sut
     }
     
-    func makeValidViewModel() -> SignUpViewModel {
-        return SignUpViewModel(name: "any_name", email: "any_email@domain.com", password: "secret", passwordConfirmation: "secret")
-    }
-    
-    func makeViewModelWithInvalidEmail() -> SignUpViewModel {
-        return SignUpViewModel(name: "any_name", email: "invalid_email@domain.com", password: "secret", passwordConfirmation: "secret")
-    }
-    
-    func makeViewModelWithoutName() -> SignUpViewModel {
-        return SignUpViewModel(email: "any_email@domain.com", password: "secret", passwordConfirmation: "secret")
-    }
-    
-    func makeViewModelWithoutEmail() -> SignUpViewModel {
-        return SignUpViewModel(name: "Gorbadock Oldbuck", password: "secret", passwordConfirmation: "secret")
-    }
-    
-    func makeViewModelWithoutPassword() -> SignUpViewModel {
-        return SignUpViewModel(name: "Gorbadock Oldbuck", email: "any_email@domain.com", passwordConfirmation: "secret")
-    }
-    
-    func makeViewModelWithoutPasswordConfirmation() -> SignUpViewModel {
-        return SignUpViewModel(name: "Gorbadock Oldbuck", email: "any_email@domain.com", password: "secret")
-    }
-    
-    func makeViewModelWithPasswordConfirmationNotMatch() -> SignUpViewModel {
-        return SignUpViewModel(name: "any_name", email: "any_email@domain.com", password: "secret", passwordConfirmation: "wrong_password")
+    func makeSignUpViewModel(name: String? = "any_name", email: String? = "any_email@domain.com", password: String? = "secret", passwordConfirmation: String? = "secret") -> SignUpViewModel {
+        return SignUpViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation)
     }
     
     class EmailValidatorSpy: EmailValidator {
         
-        var isValid = true
+        private var isValid = true
         var email: String?
         
         func isValid(email: String) -> Bool {
             self.email = email
             return isValid
+        }
+        
+        func simulateInvalidEmail() {
+            isValid = false
         }
     }
 }
