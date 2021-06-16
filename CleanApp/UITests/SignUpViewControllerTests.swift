6e9102c84709db29a8af1e0a2105a4825f4b24bb
Heitor Novais <hnovais95@gmail.com>
2021-06-16 14:22:27 -0300

    feat(projeto CleanApp): adicionar teste para garantir que SignUpViewController implemente protocolo LoadingView

--- CleanApp/UITests/SignUpViewControllerTests.swift
+++ CleanApp/UITests/SignUpViewControllerTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 import UIKit
 @testable import UI
+import Presentation
 
 class SignUpViewControllerTests: XCTestCase {
 
@@ -16,5 +17,11 @@ class SignUpViewControllerTests: XCTestCase {
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
         sut.loadViewIfNeeded()
         XCTAssertEqual(sut.loadingIndicator.isAnimating, false)
+    }
+    
+    func test_sut_implements_loadingView() {
+        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        XCTAssertNotNil(sut as LoadingView)
     }
 }
