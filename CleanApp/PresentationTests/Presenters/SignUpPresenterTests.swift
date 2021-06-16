ea3d260d7dea1d5cae59107375268d086c133aef
Heitor Novais <hnovais95@gmail.com>
2021-06-16 10:48:12 -0300

    feat(projeto CleanApp): adicionar teste para garantir que não há vazamento de memória

--- CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
+++ CleanApp/PresentationTests/Presenters/SignUpPresenterTests.swift
@@ -117,8 +117,9 @@ class SignUpPresenterTests: XCTestCase {
 
 extension SignUpPresenterTests {
     
-    func makeSut(alertView: AlertView = AlertViewSpy(), emailValidator: EmailValidator = EmailValidatorSpy(), addAccount: AddAccountSpy = AddAccountSpy()) -> SignUpPresenter {
+    func makeSut(alertView: AlertView = AlertViewSpy(), emailValidator: EmailValidator = EmailValidatorSpy(), addAccount: AddAccountSpy = AddAccountSpy(), file: StaticString = #file, line: UInt = #line) -> SignUpPresenter {
         let sut = SignUpPresenter(alertView: alertView, emailValidator: emailValidator, addAccount: addAccount)
+        checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }
     
