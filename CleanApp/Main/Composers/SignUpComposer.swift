52e7686d48331b4c557c09fbb9d87817cc12a8ae
Heitor Novais <hnovais95@gmail.com>
2021-06-17 12:10:57 -0300

    refactor(projeto CleanApp): mover classe para sua própria pasta

--- CleanApp/Main/Composers/SignUpComposer.swift
+++ CleanApp/Main/Composers/SignUpComposer.swift
@@ -7,34 +7,10 @@
 
 import Domain
 import UI
-import Presentation
 
 public final class SignUpComposer {
     
     public static func composeWith(addAccount: AddAccount) -> SignUpViewController {
         return ControllerFactory.makeSignUp(addAccount: addAccount)
-    }
-}
-
-class WeakVarProxy<T: AnyObject> {
-    
-    private weak var instance: T?
-    
-    init(_ instance: T) {
-        self.instance = instance
-    }
-}
-
-extension WeakVarProxy: AlertView where T: AlertView {
-    
-    func showMessage(viewModel: AlertViewModel) {
-        instance?.showMessage(viewModel: viewModel)
-    }
-}
-
-extension WeakVarProxy: LoadingView where T: LoadingView {
-    
-    func display(viewModel: LoadingViewModel) {
-        instance?.display(viewModel: viewModel)
     }
 }
