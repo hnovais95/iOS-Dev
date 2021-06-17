6f59d2b6fd3a80c217f96817d1a8741f0f3f0269
Heitor Novais <hnovais95@gmail.com>
2021-06-17 09:35:44 -0300

    feat(projeto CleanApp): adicionar camada de validação

---
+++ CleanApp/ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Heitor Novais | Gerencianet on 17/06/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+    }
+}
