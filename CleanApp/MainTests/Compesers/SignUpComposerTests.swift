d76d7ea849896c6f54bc9ab5aa11fa2ce2bae123
Heitor Novais <hnovais95@gmail.com>
2021-06-17 14:22:26 -0300

    refactor(projeto CleanApp): mover criação de sut para helper

--- CleanApp/MainTests/Compesers/SignUpComposerTests.swift
+++ CleanApp/MainTests/Compesers/SignUpComposerTests.swift
@@ -7,11 +7,23 @@
 
 import XCTest
 import Main
+import UI
 
 class SignUpComposerTests: XCTestCase {
 
-    func test_ui_presentation_integration() {
-        let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
-        checkMemoryLeak(for: sut)
+    func test_background_request_should_complete_on_main_thread() {
+        let (sut, _) = makeSut()
+        sut.loadViewIfNeeded()
+    }
+}
+
+extension SignUpComposerTests {
+    
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (SignUpViewController, AddAccountSpy) {
+        let addAccountSpy = AddAccountSpy()
+        let sut = SignUpComposer.composeControllerWith(addAccount: addAccountSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAccountSpy, file: file, line: line)
+        return (sut, addAccountSpy)
     }
 }
