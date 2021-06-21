d016b48f23b958b6fc717567434648cf3b778e06
Heitor Novais <hnovais95@gmail.com>
2021-06-21 12:52:06 -0300

    feat(projeto CleanApp): adicionar testes para garantir a tela está iniciando com o loading escondido, que a tela está implementando os protocolos LoadingView e AlertView e que está executando corretamente o método de login

---
+++ CleanApp/UITests/Scenes/Login/LoginViewControllerTests.swift
@@ -0,0 +1,47 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Heitor Novais | Gerencianet on 16/06/21.
+//
+
+import XCTest
+import UIKit
+@testable import UI
+import Presentation
+
+class LoginViewControllerTests: XCTestCase {
+
+    func test_loading_is_hidden_on_start() {
+        XCTAssertEqual(makeSut().loadingIndicator.isAnimating, false)
+    }
+    
+    func test_sut_implements_loadingView() {
+        XCTAssertNotNil(makeSut() as LoadingView)
+    }
+    
+    func test_sut_implements_alertView() {
+        XCTAssertNotNil(makeSut() as AlertView)
+    }
+    
+    func test_loginButton_calls_login_on_tap() {
+        var loginViewModel: LoginViewModel?
+        let sut = makeSut(loginSpy: { loginViewModel = $0 })
+        sut.loginButton?.simulateTap()
+        
+        let email = sut.emailTextField.text
+        let password = sut.passwordTextField.text
+        
+        XCTAssertEqual(loginViewModel, LoginViewModel(email: email, password: password))
+    }
+}
+
+extension LoginViewControllerTests {
+    
+    func makeSut(loginSpy: ((LoginViewModel) -> Void)? = nil) -> LoginViewController {
+        let sut = LoginViewController.instantiate()
+        sut.login = loginSpy
+        sut.loadViewIfNeeded()
+        return sut
+    }
+}
