b13aae7fb25a04101f48cb013b185b091a92e807
Heitor Novais <hnovais95@gmail.com>
2021-06-21 15:47:07 -0300

    refactor(projeto CleanApp): setar WelcomeViewController como tela inicial

--- CleanApp/Main/SceneDelegate.swift
+++ CleanApp/Main/SceneDelegate.swift
@@ -11,15 +11,26 @@ import UI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    private let signUpFactory: () -> SignUpViewController = {
+        let remoteAddAccount = makeRemoteAddAccount(httpClient: makeAlamofireAdapter())
+        return makeSignUpController(addAccount: remoteAddAccount)
+    }
+    
+    private let loginFactory: () -> LoginViewController = {
+        let remoteAuthentication = makeRemoteAuthentication(httpClient: makeAlamofireAdapter())
+        return makeLoginController(authentication: remoteAuthentication)
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let nav = NavigationController()
-        let httpClient = makeAlamofireAdapter()
-        let addAccount = makeRemoteAddAccount(httpClient: httpClient)
-        let signUpController = makeSignUpController(addAccount: addAccount)
-        nav.setRootViewController(signUpController)
+        let welcomeRouter = WelcomeRouter(nav: nav, loginFactory: loginFactory, signUpFactory: signUpFactory)
+        let welcomeViewController = WelcomeViewController.instantiate()
+        welcomeViewController.signUp = welcomeRouter.gotoSignUp
+        welcomeViewController.login = welcomeRouter.gotoLogin
+        nav.setRootViewController(welcomeViewController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
